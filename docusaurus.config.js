6bda6ac48c7521e4c6e442ff87a45f71fd8401b9
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2024-05-25 15:12:20 -0500

    Font customization

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -192,6 +192,33 @@ const config = {
       //   darkTheme: darkCodeTheme,
       // },
     }),
+    // <link rel="preconnect" href="https://fonts.googleapis.com">
+    // <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    // <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    headTags: [
+      {
+        tagName: 'link',
+        attributes: {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+      },
+      {
+        tagName: 'link',
+        attributes: {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous'
+        },
+      },
+      {
+        tagName: 'link',
+        attributes: {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
+        },
+      }
+    ]
 };
 
 export default config;
