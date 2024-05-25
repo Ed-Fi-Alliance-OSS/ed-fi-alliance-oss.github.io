48c6f4c4902d3b2639fee3d4f30f5f67d06a5131
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2024-05-24 22:13:05 -0500

    Begin customizing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,22 +6,25 @@
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
+// const lightCodeTheme = require('prism-react-renderer/themes/github');
+// const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Ed-Fi Tech',
+  tagline: 'Connecting student data. Once. And for all.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ed-fi-alliance-oss.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ed-fi-alliance-oss', // Usually your GitHub org/user name.
+  projectName: 'ed-fi-alliance-oss.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -44,14 +47,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ed-fi-alliance-oss/ed-fi-alliance-oss.github.io/tree/main/packages/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ed-fi-alliance-oss/ed-fi-alliance-oss.github.io/tree/main/blog/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -66,26 +69,75 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Ed-Fi Alliance ',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Ed-Fi Docs Logo',
+          src: 'img/logo.png',
         },
         items: [
+          // {to: '/getting-started', label: 'Getting Started', position: 'left'},
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'API Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            type: 'docsVersionDropdown',
+            position: 'left',
+          },
+          {to: '/blog', label: 'Developer Blog', position: 'left'},
+          {
+            href: 'https://github.com/Ed-Fi-Alliance-OSS/',
             label: 'GitHub',
+            position: 'right',
+          },
+          {
+            href: 'https://success.ed-fi.org/',
+            label: 'Community',
+            position: 'right',
+          },
+          {
+            href: 'https://academy.ed-fi.org/',
+            label: 'Academy',
             position: 'right',
           },
         ],
       },
+      // algolia: {
+      //   // The application ID provided by Algolia
+      //   appId: 'FO340LC414',
+
+      //   // Public API key: it is safe to commit it
+      //   apiKey: 'b6725a50a8f5cc2d56e128cd62b5ef1f',
+
+      //   indexName: 'teched-fi',
+
+      //   // Optional: see doc section below
+      //   contextualSearch: true,
+
+      //   // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      //   externalUrlRegex: 'external\\.com|domain\\.com',
+
+      //   // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      //   replaceSearchResultPathname: {
+      //     from: '/docs/', // or as RegExp: /\/docs\//
+      //     to: '/',
+      //   },
+
+      //   // Optional: Algolia search parameters
+      //   searchParameters: {},
+
+      //   // Optional: path for search page that enabled by default (`false` to disable it)
+      //   searchPagePath: 'search',
+
+      //   placeholder: 'Ask me something',
+
+      //   //... other Algolia params
+      //   headerLinks: [
+      //     { search: true }
+      //   ],
+      // },
       footer: {
         style: 'dark',
         links: [
@@ -102,16 +154,16 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'Ed-Fi Academy',
+                href: 'https://academy.ed-fi.org',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: 'Ed-Fi Success',
+                href: 'https://success.ed-fi.org',
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: 'Ed-Fi Slack',
+                href: '#',
               },
             ],
           },
@@ -119,22 +171,26 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: 'News',
+                to: 'https://www.ed-fi.org/blog',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Ed-Fi GitHub',
+                href: 'https://github.com/Ed-Fi-Alliance-OSS/',
+              },
+              {
+                label: 'Ed-Fi Events',
+                href: 'https://www.ed-fi.org/events/',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Ed-Fi Alliance.`,
       },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
+      // prism: {
+      //   theme: lightCodeTheme,
+      //   darkTheme: darkCodeTheme,
+      // },
     }),
 };
 
