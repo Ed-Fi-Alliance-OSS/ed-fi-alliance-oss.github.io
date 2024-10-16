13263b609717daf322388e4e3563649e021f6fb9
Stephen A. Fuqua <stephen@safnet.com>
2024-10-16 08:55:05 -0500

    Support for mobile sidebar Extract version info to shared component

---
+++ src/components/VersionDropDown/index.jsx
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+import { useLocation } from '@docusaurus/router';
+
+const odsApi = [
+  {
+    path: '/docs/ods-api',
+    number: '7.2',
+  },
+  {
+    path: '/docs/ods-api/6.2',
+    number: '6.2',
+  },
+  {
+    path: '/docs/ods-api/5.4',
+    number: '5.4',
+  },
+];
+
+export default function VersionDropDown() {
+  const { pathname } = useLocation();
+
+  let product = null;
+  let versions = [];
+  if (pathname.includes('/ods-api/')) {
+    product = 'ODS/API';
+    versions = odsApi;
+  }
+
+  if (product === null) {
+    return '';
+  }
+  return (
+    <div className="dropdown dropdown--hoverable margin-top--md margin-left--md">
+      <button className="button button--secondary navbar__link">
+        {product} Versions
+      </button>
+      <ul className="dropdown__menu">
+        {versions.map((version, index) => (
+          <li key={index}>
+            <a className="dropdown__link" href={'/docs/' + version.path}>
+              v{version.number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/theme/DocSidebar/Desktop/Content/index.js
+++ src/theme/DocSidebar/Desktop/Content/index.js
@@ -4,57 +4,14 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 import React from 'react';
-import {useLocation} from '@docusaurus/router';
 import Content from '@theme-original/DocSidebar/Desktop/Content';
-
-
-const odsApi = [
-    {
-      path: '/docs/ods-api',
-      number: '7.2'
-    },
-    {
-      path: '/docs/ods-api/6.2',
-      number: '6.2'
-    },
-    {
-      path: '/docs/ods-api/5.4',
-      number: '5.4'
-    }
-  ];
-
-function VersionDropDown(props) {
-  return (
-    <div className="dropdown dropdown--hoverable margin-top--md margin-left--md">
-    <button className="button button--secondary navbar__link">
-      {props.product} Versions
-    </button>
-    <ul className="dropdown__menu">
-    {props.versions.map((version, index) => (
-      <li key={index}>
-        <a className="dropdown__link" href={ "/docs/" + version.path }>
-          v{version.number}
-        </a>
-      </li>
-    ))}
-    </ul>
-  </div>
-  );
-}
+import VersionDropDown from '../../../../components/VersionDropDown';
 
 export default function ContentWrapper(props) {
-  const {pathname} = useLocation();
 
-  if (pathname.includes('/ods-api/')) {
-    return (
-      <>
-        <VersionDropDown product="ODS/API" versions={odsApi} />
-        <Content {...props} />
-      </>
-    );
-  }
   return (
     <>
+      <VersionDropDown />
       <Content {...props} />
     </>
   );
