9e637c789ae1701044b25513e2ffa9d081694049
Stephen A. Fuqua <stephen@safnet.com>
2025-04-05 15:06:04 -0500

    Improved reference page

--- src/pages/reference.jsx
+++ src/pages/reference.jsx
@@ -7,50 +7,6 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
-import Admonition from '@theme/Admonition';
-
-const LinkList = [
-  {
-    title: 'Data Exchange Standards',
-    to: '/reference/data-exchange',
-  },
-  {
-    title: 'ODS/API Platform',
-    to: '/reference/ods-api-platform',
-  },
-  {
-    title: 'Admin API',
-    to: '/reference/admin-api',
-  },
-  {
-    title: 'MetaEd',
-    to: '/reference/metaed',
-  },
-  {
-    title: 'API Publisher',
-    to: '/reference/api-publisher',
-  },
-  {
-    title: 'Data Import',
-    to: '/reference/data-import',
-  },
-  {
-    title: 'Admin App',
-    to: '/reference/admin-app',
-  },
-  {
-    title: 'Docker Deployments',
-    to: '/reference/docker',
-  },
-];
-
-function ListItem({ title, to }) {
-  return (
-    <li>
-      <Link to={to}>{title}</Link>
-    </li>
-  );
-}
 
 function Main() {
   return (
@@ -58,13 +14,100 @@ function Main() {
       <div className="container">
         <div className="row margin-top--lg">
           <div className="col">
-            <Admonition type="tip">
-              Reference landing page content coming soon. For now, just
-              providing basic links out to component pages.
-            </Admonition>
-            {LinkList.map((props, idx) => (
-              <ListItem key={idx} {...props} />
-            ))}
+            <p>
+              The Ed-Fi technology suite is a comprehensive set of tools
+              designed to empower educators and administrators by providing a
+              unified view of educational data. Explore our suite to learn how
+              we can help you streamline data management and enhance educational
+              outcomes.
+            </p>
+            <h2>Ed-Fi Technology Roadmap</h2>
+            <p>
+              Stay up to date with the latest developments and future plans for
+              the Ed-Fi technology suite. Our roadmap provides detailed
+              information on upcoming releases and features.{' '}
+              <Link to="/reference/roadmap/">
+                Learn more about the Ed-Fi Technology Roadmap
+              </Link>
+            </p>
+            <h2>Ed-Fi Data Exchange Standards</h2>
+            <p>
+              The Ed-Fi Alliance maintains the Ed-Fi Data Standard, a common
+              language that allows educational technology systems to securely
+              exchange data. It helps educators and administrators gain a
+              comprehensive understanding of student progress and make informed
+              decisions. The Ed-Fi API Standard define criteria for creation of
+              HTTP-based API applications that use or support the Ed-Fi Data
+              Standard.{' '}
+              <Link to="/reference/data-exchange">
+                Discover the Ed-Fi Data Exchange Standards
+              </Link>
+            </p>
+            <h2>Ed-Fi ODS/API</h2>
+            <p>
+              The Ed-Fi ODS/API provides a robust platform for integrating and
+              managing educational data through a REST-inspired HTTP interface.
+              It enables seamless data exchange and supports various educational
+              applications.{' '}
+              <Link to="/reference/ods-api">Explore the Ed-Fi ODS/API</Link>
+            </p>
+            {/* useful text for future use, not ready to include here yet. */}
+            {/* <h2>Ed-Fi Data Management Service</h2>
+            <p>
+              The Ed-Fi Data Management Service (DMS) is the next-generation
+              solution for managing educational data. It offers enhanced
+              performance and scalability, making it easier to handle large
+              datasets. Learn more about the Ed-Fi Data Management Service
+            </p> */}
+            <h2>Ed-Fi Admin API</h2>
+            <p>
+              The Ed-Fi Admin API provides an administrative interface for
+              managing the Ed-Fi ODS/API platform. It simplifies common
+              administrative tasks and supports both Docker and Windows
+              installations.{' '}
+              <Link to="/reference/admin-api">
+                Get started with the Ed-Fi Admin API
+              </Link>
+            </p>
+            <h2>Ed-Fi API Publisher</h2>
+            <p>
+              The Ed-Fi API Publisher is a utility for moving data between Ed-Fi
+              ODS/API instances. It supports various deployment models and
+              ensures secure data transfer.{' '}
+              <Link to="/reference/api-publisher">
+                Learn about the Ed-Fi API Publisher
+              </Link>
+            </p>
+            <h2>MetaEd</h2>
+            <p>
+              MetaEd is a technology framework that uses a domain-specific
+              language to auto-generate software, database, and data standard
+              artifacts. It supports the development of Ed-Fi extensions and
+              core components.{' '}
+              <Link to="/reference/metaed">Discover MetaEd</Link>
+            </p>
+            <h2>Other Ed-Fi Tools</h2>
+            <ul>
+              <li>
+                <Link to="/reference/educator-pipeline">
+                  Educator Pipeline Dashboards
+                </Link>
+              </li>
+              <li>
+                <Link to="/reference/docker">Sample Docker Deployments</Link>
+              </li>
+              <li>
+                <Link to="/reference/data-import">Data Import</Link>
+              </li>
+              <li>
+                <Link to="/reference/admin-app">Admin App</Link>
+              </li>
+              <li>
+                <Link to="/reference/analytics-middle-tier">
+                  Analytics Middle Tier
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
