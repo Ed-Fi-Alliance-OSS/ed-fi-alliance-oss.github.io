ce870f7dbc14009fe6e37189061b69eab6cf3632
Nathan Gandomi <108550600+ngandomiEF@users.noreply.github.com>
2025-07-24 15:40:17 -0500

    Updateing Aeries certification data to include new  verifying agencies and updated documentation links.

--- docs/partners/2-certification/certificationData.js
+++ docs/partners/2-certification/certificationData.js
@@ -9,15 +9,17 @@ export default {
       productName: 'Aeries',
       logo: 'https://edfidocs.blob.core.windows.net/$web/img/partners/certification/Aeries_logo.png',
       website: 'https://www.aeries.com/',
-      validThrough: 'April 30, 2025',
-      verifyingAgencies: ['Edna ISD (TX)', 'Moody ISD (TX)'],
+      validThrough: 'August 1, 2026',
+      verifyingAgencies: ['Edna ISD (TX)', 'Moody ISD (TX)', 'Keller ISD (TX)'],
       versionCompatibility:
         'Conformance testing met with Aeries single product version (distributed to all customers)',
       availability: 'Available to any agency (all sizes) in the U.S.',
       documentation: {
         'Availability Statement':
-          'https://edfi.atlassian.net/wiki/download/attachments/23694066/Aeries%20v4%20Ed-Fi%20Certification%20-%20Availability%20Statement.docx?version=1&modificationDate=1682594251897&cacheVersion=1&api=v2',
-        Mappings:
+          'https://edfidocs.blob.core.windows.net/$web/assets/partners/certification/Aeries/Ed-Fi%20Cert%20-%20Availabiity%20Statement%2007.25.pdf',
+        'Pricing Statement':
+          'https://edfidocs.blob.core.windows.net/$web/assets/partners/certification/Aeries/Ed-Fi%20Pricing%20Statement%20July_2025_signed.pdf',
+        'Mappings':
           'https://edfi.atlassian.net/wiki/download/attachments/23694066/Ed-Fi%20Mappings%2010-21.xlsx?version=1&modificationDate=1668719714227&cacheVersion=1&api=v2',
       },
       supportResources: [
