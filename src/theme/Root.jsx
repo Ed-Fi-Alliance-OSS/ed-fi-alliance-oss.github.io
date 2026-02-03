d86b7b41bb0b25e6da4b971d7a9f608fe2174524
Stephen A. Fuqua <stephen@safnet.com>
2026-02-02 21:13:29 -0600

    Remove unused using

--- src/theme/Root.jsx
+++ src/theme/Root.jsx
@@ -3,7 +3,6 @@
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
 
-import React from 'react';
 import ChatbotWidget from '@site/src/components/ChatbotWidget';
 
 /**
